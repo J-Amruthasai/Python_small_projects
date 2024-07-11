36ad2eb98e1a79a11de99394a89a0c2645fb9fe1
J-Amruthasai <105196917+J-Amruthasai@users.noreply.github.com>
2024-07-11 10:15:54 +0530

    Create game.py

---
+++ Quiz_game_with_UI_API/game.py
@@ -0,0 +1,9 @@
+from question_model import Question
+from data import questions
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+question_bank = [Question(question["question"], question["correct_answer"]) for question in questions]
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
